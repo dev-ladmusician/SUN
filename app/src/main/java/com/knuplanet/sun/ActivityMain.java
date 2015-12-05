00b0fd229f5ddb8355bee691f7913c7d64e209a5
김동진 <ladmusician.kim@gmail.com>
2015-12-05 16:52:21 +0900

    main page design

--- app/src/main/java/com/knuplanet/sun/ActivityMain.java
+++ app/src/main/java/com/knuplanet/sun/ActivityMain.java
@@ -1,9 +1,29 @@
 package com.knuplanet.sun;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.OnClick;
 
 public class ActivityMain extends AppCompatActivity {
+    @Bind(R.id.main_btn_login)
+    Button mBtnLogin;
+    @Bind(R.id.main_btn_join)
+    Button mBtnJoin;
+
+    @OnClick({ R.id.main_btn_login, R.id.main_btn_join }) void onClick(Button btn) {
+        switch(btn.getId()) {
+            case R.id.main_btn_join:
+
+                break;
+            case R.id.main_btn_login:
+                break;
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
